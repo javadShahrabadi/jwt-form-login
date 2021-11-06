b09159e5dac6c554906bb1fc3ae97c41efa9a8de
javadShahrabadi <emamafi78@protonmail.com>
2021-11-06 16:22:54 +0330

    publish version

---
+++ controllers/main.js
@@ -0,0 +1,25 @@
+// main controller
+require("dotenv").config();
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+const { BadRequestError } = require("../errors");
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new BadRequestError("please provide username and password!");
+  }
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: "user created.", token });
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username} `,
+    secret: `Here is your authorized data, your luckyNumber is ${luckyNumber}`,
+  });
+};
+module.exports = { login, dashboard };
